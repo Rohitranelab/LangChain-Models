a894ecb0ae40e8b427da945a301188bc09d12db9
Rohit <ranerohit996@gmail.com>
2026-09-08 16:33:59 +0530

    try anthropic chat model

--- 2.ChatModels/chatmodel_openai.py
+++ 2.ChatModels/chatmodel_openai.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatOpenAI(model = "gpt-4o-2024-08-06")
+model = ChatOpenAI(model = "gpt-4o-2024-08-06", temperature = 0, max_completion_tokens = 10)
 
 result = model.invoke("What is the captial of India")
 
