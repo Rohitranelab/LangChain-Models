8035b682a35512453383aacc76c4edfd58e926d4
Rohit <ranerohit996@gmail.com>
2026-09-09 11:25:52 +0530

    create document similarity

---
+++ 3.EmbeddingModels/embedding_hf_local.py
@@ -0,0 +1,12 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedding = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+
+text = "Delhi is the captial of India"
+
+result = embedding.embed_query(text)
+
+print(str(result))
