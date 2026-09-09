15a1a1b5cfcb9e2f93c9cfa1b039c7a9b3b0642c
Rohit <ranerohit996@gmail.com>
2026-09-09 10:56:28 +0530

    try different model

---
+++ 3.EmbeddingModels/embedding_openai_query.py
@@ -0,0 +1,10 @@
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings(model = "text-embedding-3-small", dimensions = 32)
+
+result = embeddings.embed_query("Delhi is the captial of India")
+
+print(str(result))
